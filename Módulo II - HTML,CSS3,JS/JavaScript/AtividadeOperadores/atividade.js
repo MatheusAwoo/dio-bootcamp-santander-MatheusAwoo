637cbefe964270f9d09c6f881776f1bd25d0a7e6
Matheus Mauricio <nakashi00@live.com>
2022-07-25 13:37:05 -0300

    Atualizando arquivos

---
+++ Módulo II - HTML,CSS3,JS/JavaScript/AtividadeOperadores/atividade.js
@@ -0,0 +1,10 @@
+// Criar Função que receba dois números como valores.
+// Que confira se os números são iguais
+// Que confira se a soma dos números são maior que 10 ou menor que 20
+// Retorne uma string com "Os números num1 e num2 não são iguais. Sua soma é , que é maior que 10
+// e menor que 20"
+
+function equal(num1, num2){
+    const areEqual = num1 === num2;
+    
+}
